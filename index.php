3e76e981aa003a788b21c8d7414ea74c71a15599
Dot3sudiogithub <codemonk22233@gmail.com>
2020-05-26 16:36:09 +0530

    home page front end development complete

--- index.php
+++ index.php
@@ -7,11 +7,13 @@
     <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/all.css">
+    <link rel="stylesheet" href="css/aos.css">
     <link rel="stylesheet" href="css/style.css">
 
     <script src="js/jquery-3.5.1.js"></script>
     <script src="js/bootstrap.js"></script>
     <script src="js/all.js"></script>
+    <script src="js/aos.js"></script>
     <script src="js/typed.js"></script>
     </head>
 <body onload="myfunction()">
@@ -65,13 +67,184 @@
   <div class="container">
   <div class="row">
   <div class="col-md-6">
-  <h1 class="display-4 text-center text-secondary">Welcome</h1>
+  <h1 class="display-4 text-center text-light">Welcome</h1>
     <div class="text-center">
   <div class="display-4 text-warning">DOT3STUDIO</div>
-  <div class="h1 text-light">WE ARE <span class="type text-warning h1"></span></div>
+  <div class="h3 text-light">WE ARE <span class="type text-warning h3"></span></div>
   <a  href="portfolio.php" class="btn btn-outline-primary">DISCOVER ME</a>
 
-  <ul class="nav justify-content-center my-5">
+  
+
+</div>
+  </div>
+  <div class="col-md-6 text-center mt-3">
+  <img src="images/group.png" alt="" width="350px" height="350px">
+  </div>
+  </div>
+
+  </div>
+</div>
+<!-- end hero area  -->
+
+
+<!-- start post area  -->
+<div class="container-fluid">
+<div class="row">
+  <div class="col-md-9">
+    <div class="h3">Recent Blog</div>
+    <div class="row my-5">
+      <div class="col-md-4">
+        <div class="card cardd">
+          <div class="card-body boxx">
+            <img src="images/1.png" alt="" class="card-img-top image">
+            <div class="middle">
+              <div class="text">Blog title</div>
+            </div>
+          </div><!-- end card body -->
+        </div>
+      </div>
+      <div class="col-md-4"></div>
+      <div class="col-md-4"></div>
+    </div>
+  </div>
+  <!-- end col-9  -->
+  <div class="col-md-3">
+    <div class="card">
+      <div class="card-header bg-secondary text-light">Category</div>
+      <div class="card-body">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a href="#" class="nav-link">Recent Blog</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Top Blog</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Blog By Cource</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Blog By date</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!-- end card  -->
+    <div class="card">
+      <div class="card-header bg-secondary text-light">Blog Archive</div>
+      <div class="card-body">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a href="#" class="nav-link">May 2020</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">June 2020</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">July 2020</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Auguest 2020</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+</div>
+<!-- end col-3  -->
+</div>
+</div>
+<!-- end post area  -->
+
+<!-- about us area start -->
+<div class="container-fluid aboutus_section">
+  <div class="h3 text-center text-uppercase my-5 text-light">what do we offer</div>
+  <div class="row">
+  <div class="col-md-6" >
+     <div class="text-center" data-aos="fade-right">
+       <div class="h1 my-5 text-light">about my skills</div>
+       <div>
+         <img src="images/Lantern.png" alt="" width="250px" height="250px" class="rounded-circle">
+       </div>
+     </div>
+    </div>
+    <!-- end col 6 for text  -->
+    <div class="col-md-6">
+      <div class="">
+        <div class="card bg-transparent border-0" data-aos="fade-up">
+          <div class="card-body">
+          <div class="my-4">
+                    <label for="" class="text-success">HTML</label>
+
+                    <div class="progress">
+                    <div class="progress-bar progress-bar-striped bg-success progress-bar-animated myhtml " role="progressbar" style="width: 0%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">95%</div>
+                    </div>
+                </div>
+
+                <div class="my-4">
+                    <label for="" class="text-warning">CSS</label>
+
+                    <div class="progress">
+                    <div class="progress-bar progress-bar-striped bg-warning progress-bar-animated mycss" role="progressbar" style="width: 0%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">90                          %</div>
+                    </div>
+                </div>
+
+                <div class="my-4">
+                    <label for="" class="text-info">JAVASCRIPT</label>
+
+                    <div class="progress">
+                    <div class="progress-bar progress-bar-striped bg-info progress-bar-animated myjs" role="progressbar" style="width: 0%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">75%</div>
+                    </div>
+                </div>
+
+                <div class="my-4">
+                    <label for="" class="text-danger">PHP</label>
+
+                    <div class="progress">
+                    <div class="progress-bar progress-bar-striped bg-danger progress-bar-animated myphp" role="progressbar" style="width: 0%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">65%</div>
+                    </div>
+                </div>
+
+                <div class="my-4">
+                    <label for="" class="text-primary">Adobe XD</label>
+
+                    <div class="progress">
+                    <div class="progress-bar progress-bar-striped bg-primary progress-bar-animated myxd" role="progressbar" style="width: 0%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">75%</div>
+                    </div>
+                </div>
+               
+                <div class="my-4">
+                    <label for="" class="text-danger">Adobe illustrator</label>
+
+                    <div class="progress">
+                    <div class="progress-bar progress-bar-striped bg-danger progress-bar-animated myill" role="progressbar" style="width: 0%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">70%</div>
+                    </div>
+                </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end col-6 for image  -->
+  
+  </div>
+</div>
+<!-- end about us area  -->
+
+<!-- start connect area  -->
+
+<div class="container" id="find-us">
+  <div class="row">
+  <div class="col-md-6 pb-5">
+     <div class="text-center" data-aos="">
+       <div class="h1 text-dark text-uppercase" id="find-text"> Also Touch with us </div>
+       <img src="images/1.png" alt="" width="200px" height="200px" class="rounded-circle">
+       <div>
+        
+       </div>
+     </div>
+    </div>
+    <!-- end col-6 text  -->
+    <div class="col-md-6 bg- text-center">
+      <div class="" id="social-iconss">
+      <ul class="nav mx-auto my-5">
             <li class="nav-item">
     <a class="nav-link social-link1 ml-2" href="#">
         <i class="fab fa-facebook-f fa-2x text-light" aria-hidden="true"></i>
@@ -100,88 +273,30 @@
   </li>
  
 </ul>
-
-</div>
-  </div>
-  <div class="col-md-6 text-center mt-3">
-  <img src="images/group.png" alt="" width="350px" height="350px">
-  </div>
-  </div>
-
-  </div>
-</div>
-<!-- end hero area  -->
-
-
-<!-- start post area  -->
-<div class="container-fluid">
-<div class="row">
-  <div class="col-md-9">
-    <div class="h3">Recent Post</div>
-    <div class="row my-5">
-      <div class="col-md-4">
-        <div class="card">
-          <div class="card-body boxx">
-            <img src="images/1.png" alt="" class="card-img-top image">
-            <div class="middle">
-              <div class="text">post title</div>
-            </div>
-          </div><!-- end card body -->
-        </div>
-      </div>
-      <div class="col-md-4"></div>
-      <div class="col-md-4"></div>
-    </div>
-  </div>
-  <!-- end col-9  -->
-  <div class="col-md-3">
-    <div class="card">
-      <div class="card-header bg-secondary text-light">Category</div>
-      <div class="card-body">
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a href="#" class="nav-link">Recent Post</a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">Top Post</a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">Post By Cource</a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">Post By date</a>
-          </li>
-        </ul>
       </div>
     </div>
-    <!-- end card  -->
-    <div class="card">
-      <div class="card-header bg-secondary text-light">Post Archive</div>
-      <div class="card-body">
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a href="#" class="nav-link">May 2020</a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">June 2020</a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">July 2020</a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">Auguest 2020</a>
-          </li>
-        </ul>
+    <!-- end col-6 map  -->
+  </div>
+</div>
+<!-- end connect area  -->
+
+<!-- start footer area  -->
+
+<div class="container-fluid mt-5">
+  <div class="row">
+    <div class="col-md-12 text-center">
+      <div class="h6">
+        copyright &copy; 2020. All Right Reserved Dot3studio.
       </div>
     </div>
-</div>
-<!-- end col-3  -->
-</div>
+  </div>
 </div>
 
 
 
-<!-- end post area  -->
+<!-- end footer area  -->
+
+
 
 
 
@@ -200,6 +315,22 @@ var typed = new Typed('.type', {
     loop: true
   });
 
+</script>
+<script>
+$(document).ready(function(){
+$('.myhtml').animate({width: '95%'}, 200);
+$('.mycss').animate({width: '90%'}, 200);
+$('.myjs').animate({width: '75%'}, 200);
+$('.myphp').animate({width: '65%'}, 200);
+$('.myxd').animate({width: '65%'}, 200);
+$('.myill').animate({width: '65%'}, 200);
+
+});
+
+</script>
+
+<script>
+  AOS.init();
 </script>
 
 </body>
