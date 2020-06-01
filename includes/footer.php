4d3b250c49ac497c88cdee176705f3a06d6134ba
Dot3sudiogithub <codemonk22233@gmail.com>
2020-06-01 11:07:03 +0530

    restart work

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+<!-- start footer area  -->
+
+<div class="container-fluid mt-5">
+  <div class="row">
+    <div class="col-md-12 text-center">
+      <div class="h6">
+        copyright &copy; 2020. All Right Reserved Dot3studio.
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<!-- end footer area  -->
+</body>
+</html>
